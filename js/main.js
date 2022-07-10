63e5f6b0b38d83a6a0070f3e441f215174df941f
mi nombre <desarrollowebaps@gmail.com>
2022-07-09 22:39:26 -0400

    Actualizando proyecto

--- js/main.js
+++ js/main.js
@@ -2,43 +2,74 @@ let baseTemp = document.querySelector("#baseTemp")
 let template_parrafo = document.querySelector("#template_parrafo").content
 let fragment_parrafo = document.createDocumentFragment()
 
+let bottom = document.querySelector("#bottom")
+
 
 window.document.addEventListener("DOMContentLoaded", ()=>{
     dataFetchParafo()
 })
 
 
-/*document.addEventListener("click", (e)=>{
-    if(e.target.id === "bottom"){
-        dataFetchParafo()
-    }
-})*/
-
 
 const dataFetchParafo = async () =>{
     try{
-        resParraf = await fetch("https://api.adviceslip.com/advice")
-        dataParraf = await resParraf.json()
+        const resParraf = await fetch("https://api.adviceslip.com/advice")
+        const dataParraf = await resParraf.json()
+        //console.log(dataParraf)
         pintarInformacion(dataParraf)
 
-        
-
+    
+       
     }catch(error){
         console.log("Error al cargar json Parraf")
     }
 }
 
-
 const pintarInformacion = (dataParraf) =>{
-    console.log(dataParraf.slip)
+    //console.log(dataParraf.slip)
     
     template_parrafo.querySelector("#numberID").textContent = dataParraf.slip.id
     template_parrafo.querySelector("#cuadro_parrafo").textContent = dataParraf.slip.advice
-
+    //let clone = template_parrafo.cloneNode(true)
     fragment_parrafo.appendChild(template_parrafo)
     baseTemp.appendChild(fragment_parrafo)
+}
+
+
+///////////GENERAR NUEVA FUNCION///
+
+const numeroAleatorio = () =>{
+    return Math.floor(Math.random() * 224) + 1;  
+}
+
+
+
+const dataFetchParafoclick = async () =>{
+    try{
+        const resParrafClick = await fetch(`https://api.adviceslip.com/advice/${numeroAleatorio()}`)
+        const dataParrafClick = await resParrafClick.json()
+        //console.log(dataParraf)
+        pintarInformacionClick(dataParrafClick)
+
+    }catch(error){
+        console.log("Error al cargar json Parraf222")
+    }
+}
+
+
+const pintarInformacionClick = (dataParrafClick) =>{
+    //console.log(dataParrafClick)
     
+    document.querySelector("#numberID").textContent = dataParrafClick.slip.id
+    document.querySelector("#cuadro_parrafo").textContent = dataParrafClick.slip.advice
     
 }
+
+
+document.addEventListener("click", (e) =>{
+    if(e.target.id === "bottom"){
+        dataFetchParafoclick()
+    }
+})
 
 
